059e75d96d4ae037961b9a3d729d6c67cc2dea8d
Kim Jae Jun <rlawowns97@naver.com>
2024-08-11 03:37:57 +0900

    #5 : add only-pull-request-files report mode
    - use promise callback method, because this script cant use top level await.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require("fs");
 
 const {GITHUB_TOKEN} = process.env;
 const octokit = github.getOctokit(GITHUB_TOKEN);
@@ -10,25 +11,66 @@ try {
   const onlyChangedRspecFile = core.getInput('only-pull-request-files');
   console.log(onlyChangedRspecFile);
 
+  if (onlyChangedRspecFile === 'true') {
+    fetchPullRequestFiles(github.context)
+      .then(response => {
+        const pullRequestFiles = response.data;
+        const pullRequestFilenames = pullRequestFiles.map(pullRequestFileInfo => extractFilename(pullRequestFileInfo.filename));
+        console.log(pullRequestFilenames);
+        return filterPullRequestFilenames(pullRequestFilenames);
+      })
+      .then(filteredPullRequestFilename => createRspecReportComment(rspecResultFilepath, filteredPullRequestFilename))
+      .then(comment => {
+        octokit.rest.issues.createComment({
+          issue_number: github.context.issue.number,
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          body: comment
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        throw new Error(`fetchPullRequestFiles failed : ${error.message}`);
+      });
+  } else {
+    const comment = createRspecReportComment(rspecResultFilepath, []);
+    octokit.rest.issues.createComment({
+      issue_number: github.context.issue.number,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      body: comment
+    });
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+// @param githubContext actions/github's context object
+async function fetchPullRequestFiles(githubContext) {
+  return await octokit.rest.pulls.listFiles({
+    owner: githubContext.repo.owner,
+    repo: githubContext.repo.repo,
+    pull_number: githubContext.issue.number
+  });
+}
+
+function filterPullRequestFilenames(pullRequestFilenames) {
+  // 1. *_spec.rb 파일에서 _spec 앞의 이름 추출
+  // 2. *.rb 파일 이름 추출
+  return pullRequestFilenames.filter(changedFilename => changedFilename.endsWith('.rb'))
+    .map(changedRubyFilename => {
+      if (changedRubyFilename.endsWith('_spec.rb')) {
+        return changedRubyFilename;
+      } else {
+        const removeExtFilename = changedRubyFilename.substring(0, changedRubyFilename.length - '.rb'.length);
+        return `${removeExtFilename}_spec.rb`;
+      }
+    });
+}
+
+function createRspecReportComment(rspecResultFilepath, pullRequestRubyFilenames) {
   const fs = require('fs');
   const results = JSON.parse(fs.readFileSync(rspecResultFilepath, 'utf8'));
-
-  let pullRequestRubyFilenames = [];
-  if (onlyChangedRspecFile === 'true') {
-    const changedFilenames = fetchPullRequestFilenames(github.context);
-    // 1. *_spec.rb 파일에서 _spec 앞의 이름 추출
-    // 2. *.rb 파일 이름 추출
-    pullRequestRubyFilenames = changedFilenames.filter(changedFilename => changedFilename.endsWith('.rb'))
-      .map(changedRubyFilename => {
-        if (changedRubyFilename.endsWith('_spec.rb')) {
-          return changedRubyFilename;
-        } else {
-          const removeExtFilename = changedRubyFilename.substring(0, changedRubyFilename.length - '.rb'.length);
-          return `${removeExtFilename}_spec.rb`;
-        }
-      });
-    console.log(pullRequestRubyFilenames);
-  }
 
   let comment = `## RSpec Test Results\n\n`;
   comment += `<table>
@@ -41,7 +83,7 @@ try {
 
   results.examples.forEach(testCaseResult => {
     if (pullRequestRubyFilenames.length !== 0) {
-      const testCaseFilename = testCaseResult.file_path.split('/').slice(-1)[0];
+      const testCaseFilename = extractFilename(testCaseResult.file_path);
       if (!pullRequestRubyFilenames.includes(testCaseFilename)) {
         // skip if not included
         console.log(`${testCaseResult.file_path} is skipped, because it is not included this pull request commits.`);
@@ -63,29 +105,9 @@ try {
   });
   comment += `</table>`;
 
-  octokit.issues.createComment({
-    issue_number: github.context.issue.number,
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    body: comment
-  });
-} catch (error) {
-  core.setFailed(error.message);
+  return comment;
 }
 
-// @param githubContext actions/github's context object
-async function fetchPullRequestFilenames(githubContext) {
-  return await octokit.pulls.listFiles({
-    owner: githubContext.repo.owner,
-    repo: githubContext.repo.repo,
-    pull_number: githubContext.issue.number
-  })
-    .then(response => {
-      const pullRequestFileInfoList = response.data;
-      return pullRequestFileInfoList.map(pullRequestFileInfo => pullRequestFileInfo.filename);
-    })
-    .catch(error => {
-      console.log(error);
-      throw new Error(`fetchPullRequestFilenames failed : ${error.message}`);
-    });
+function extractFilename(filepath) {
+  return filepath.split('/').slice(-1)[0];
 }
