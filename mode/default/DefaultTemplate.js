bdca3a45efa9e8fdf35f6dc320f83d7ba6e808d0
Kim Jae Jun <rlawowns97@naver.com>
2024-08-12 01:00:54 +0900

    #8 : add template interface

--- mode/default/DefaultTemplate.js
+++ mode/default/DefaultTemplate.js
@@ -1,74 +1,8 @@
-export class DefaultTemplate {
+import {Template} from "../Template";
+
+export class DefaultTemplate extends Template {
   constructor() {
+    super();
     this.name = "DefaultTemplate";
-    this.formatter = (template, ...args) => {
-      return template.replace(/@{([0-9]+)}/g, function (match, index) {
-        return typeof args[index] === 'undefined' ? match : args[index];
-      });
-    };
-  }
-
-  header() {
-    return `
-    ## Rspec Test Results
-    
-    <table>
-      <tr>
-        <td> rspec filepath </td>
-        <td> full description </td>
-        <td> detail error message </td>
-      </tr>
-    `;
-  }
-
-  body() {
-    return `
-      <tr>
-        <td> @{0} </td>
-        <td> @{1} </td>
-        <td>
-        
-          \`\`\`console
-          
-          @{2}
-          
-          \`\`\`
-        
-        </td>
-      </tr>
-    `;
-  }
-
-  footer() {
-    return `
-    </table>
-    `;
-  }
-
-  templateString() {
-    return `
-    ## Rspec Test Results
-    
-    <table>
-      <tr>
-        <td> rspec filepath </td>
-        <td> full description </td>
-        <td> detail error message </td>
-      </tr>
-      <tr>
-        <td> @{0} </td>
-        <td> @{1} </td>
-        <td>
-        
-          \`\`\`console
-          
-          @{3}
-          
-          \`\`\`
-        
-        </td>
-      </tr>
-    </table>
-    `;
   }
 }
