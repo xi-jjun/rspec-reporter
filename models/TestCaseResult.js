e628fc1922e6d8be01f989a20e42d65dee66d39c
Kim Jae Jun <rlawowns97@naver.com>
2025-01-28 14:50:55 +0900

    #21 : refactor - add TestResult object

---
+++ models/TestCaseResult.js
@@ -0,0 +1,7 @@
+export class TestCaseResult {
+  constructor(filepath, fullDescription, failMessage) {
+    this.filepath = filepath;
+    this.fullDescription = fullDescription;
+    this.failMessage = failMessage;
+  }
+}
--- parsers/RspecParser.js
+++ parsers/RspecParser.js
@@ -1,25 +1,30 @@
 import {FrameworkParser} from "./FrameworkParser";
+import {TestResult} from "../models/TestResult";
+import {TestCaseResult} from "../models/TestCaseResult";
 
 export class RspecParser extends FrameworkParser {
   /**
    * A method that parses a test result of rspec(JSON file)
    *
    * @param filepath {string} rspec result file path
-   * @return {Object} rspec result js object
+   * @return {TestResult} rspec result js object
    */
   parse(filepath) {
     const fs = require('fs');
     const file = fs.readFileSync(filepath, this.defaultEncodingType);
     const rspecResult = JSON.parse(file);
 
-    return rspecResult.examples
+    const testCaseResults = rspecResult.examples
       .filter(rspecCaseResult => rspecCaseResult.status === 'failed')
       .map(rspecCaseResult => {
-        return {
-          filepath: rspecCaseResult.file_path,
-          fullDescription: rspecCaseResult.full_description,
-          exceptionMessage: rspecCaseResult.exception.message
-        }
+        // TODO : rspecCaseResult.run_time (spend time)
+        return new TestCaseResult(
+          rspecCaseResult.file_path,
+          rspecCaseResult.full_description,
+          rspecCaseResult.exception.message
+        );
       });
+
+    return new TestResult(testCaseResults);
   }
 }
